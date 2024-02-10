a7b4f110db403cb18c01187119831231583ffea6
Kash-miri <120159703+Kash-miri@users.noreply.github.com>
2024-02-10 23:58:21 +0500

    Add files via upload

---
+++ Week3/CopyConstructor/CopyConstructor/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CopyConstructor
+{
+    internal class Program
+    {
+        class Student
+        {
+            public Student()
+            {
+                Console.WriteLine("Copy Constructor");
+            }
+            public Student(Student n)
+            {
+                name = n.name;
+                fsc = n.fsc;
+                ecat= n.ecat;
+            }
+            public string name;
+            public int fsc;
+            public int ecat;
+        }
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+            student.name = "Aleena";
+            Student student2 = new Student(student);
+            Console.WriteLine(student.name);
+            Console.WriteLine(student2.name);
+            Console.ReadKey();
+        }
+    }
+}
