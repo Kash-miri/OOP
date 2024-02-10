95aec361f2617d475c8dc9631429de6e9cb03c04
Kash-miri <120159703+Kash-miri@users.noreply.github.com>
2024-02-10 23:40:52 +0500

    Add files via upload

---
+++ Week1/Lab 01/File 01/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1_PD1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Console.Write("Hello World!");
+            Console.Write("Hello World!");
+            Console.ReadKey();*/
+
+
+            /*Console.WriteLine("Hello World!");
+            Console.Write("Hello World!");
+            Console.ReadKey();*/
+
+
+            /*int variable = 7;
+            Console.Write("Variable = "+variable);
+            Console.ReadKey();*/
+
+
+            /*string variable = "I am string";
+            Console.Write("String = " + variable);
+            Console.ReadKey();*/
+
+
+            /*char variable = 'A';
+            Console.Write("Character = " + variable);
+            Console.ReadKey();*/
+
+
+            /*float variable = 2.2F;
+            Console.Write("Decimal = " + variable);
+            Console.ReadKey();*/
+
+
+            /*string str;
+            str = Console.ReadLine();
+            Console.Write("You have inputed = " + str);
+            Console.ReadKey();*/
+
+
+            /*string str;
+            str = Console.ReadLine();
+            Console.WriteLine("You have inputed = " + str);
+            int num = int.Parse(str);
+            Console.Write("The number is = " + num);
+            Console.ReadKey();*/
+
+
+            /*string str;
+            str = Console.ReadLine();
+            Console.WriteLine("You have inputed = " + str);
+            float num = float.Parse(str);
+            Console.Write("The number is = " + num);
+            Console.ReadKey();*/
+
+
+            float length, area;
+            length = float.Parse(Console.ReadLine());
+            area = length * length;
+            Console.Write("Area of the square is : " + area);
+            Console.ReadKey();
+        }
+    }
+}
